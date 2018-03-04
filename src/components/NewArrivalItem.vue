9215d5050ada6698c98b17eeea35da2d9b8d4023
Nobuyasu Masujima <shushutochako@gmail.com>
2018-03-04 23:19:10 +0900

    カード表示部分の修正

--- src/components/NewArrivalItem.vue
+++ src/components/NewArrivalItem.vue
@@ -1,13 +1,23 @@
 <template>
   <li>
-    <div class="content">{{ item.message }}</div>
+    <div class="container">
+      <div class="thumbnail-container">
+        <img class="thumbnail" v-bind:src="item.thumbnailUrl">
+      </div>
+      <div class="title-container">
+        {{ item.title }}
+      </div>
+      <div class="content-container">
+        {{ item.content }}
+      </div>
+    </div>
   </li>
 </template>
 
 <script>
 export default {
   name: "NewArrivalItem",
-  props:['item'],
+  props: ["item"],
   data() {
     return {};
   }
@@ -16,10 +26,33 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.content {
+.container {
   margin: 10px;
-  background: lightblue;
+  background: #fafafa;
   width: 300px;
   height: 300px;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: column; /* Safari */
+  flex-direction: column;
+}
+.thumbnail{
+  width: 100%;
+  height: 100%;
+}
+.thumbnail-container {
+  height: 180px;
+}
+.title-container {
+  height: 40px;
+  line-height: 40px;
+  text-align: left;
+  padding-left: 16px;
+}
+.content-container {
+  height: 45px;
+  flex-grow: 1;
+  text-align: left;
+  padding-left: 16px;
 }
 </style>
--- src/components/NewArrivals.vue
+++ src/components/NewArrivals.vue
@@ -8,38 +8,41 @@
 </template>
 
 <script>
-
-import Vue from 'vue'
-import NewArrivalItem from './NewArrivalItem'
+import Vue from "vue";
+import NewArrivalItem from "./NewArrivalItem";
+import axios from 'axios'
 
 //  コンポーネントを登録
-Vue.component('newArrival-item', NewArrivalItem)
+Vue.component("newArrival-item", NewArrivalItem);
 
 export default {
   name: "NewArrivals",
   data() {
     return {
       msg: "NewArrivals",
-      items:[
-        { message: "item aa"},
-        { message: "item bb"},
-        { message: "item cc"},
-        { message: "item 4"},
-        { message: "item 5"},
-        { message: "item 6"},
-        { message: "item 6"},
-        { message: "item 6"},
-      ]
+      items: []
     };
   },
+  created: async function() {
+     const response = await axios.get('http://127.0.0.1:3000/new_arrivals')
+     .catch((err) => {
+       console.log(err);
+     });
+
+     this.items = response.data.map((elm) => {
+       return {
+         title: elm.title,
+         content: elm.content,
+         thumbnailUrl: elm.thumbnailUrl
+       }
+     });
+  },
   methods: {
-    countUp () {
-      this.counter++
+    countUp() {
+      this.counter++;
     }
   }
 };
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -62,6 +65,6 @@ a {
 .container {
   display: flex;
   flex-wrap: wrap;
-  justify-content:center;
+  justify-content: center;
 }
 </style>
